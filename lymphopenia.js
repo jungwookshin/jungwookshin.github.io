addd55cdde32c1ee9113f5ec29ab77c1b73ad963
Jungwook Shin <jungwook.shin@gmail.com>
2024-02-03 21:43:55 -0500

    visual guide lines are added
     HEAD detached at origin/main
     Changes to be committed:
    	modified:   index.html
    	modified:   lymphopenia.js

--- lymphopenia.js
+++ lymphopenia.js
@@ -30,127 +30,305 @@ starts = {pts: margin.left,
           slp: margin.left + points.slp[0]/points.tot_pts[1]*full_scale} 
 
 
-  // Create the SVG container.
+// Create the SVG container.
 const svg = d3.create("svg")
       .attr("width", width)
       .attr("height", height)
       .attr("viewBox", [0, 0, width, height])
 
-  const pts = d3.scaleLinear()
-        .domain(domain.pts)
-        .range([margin.left, margin.left + full_scale ]);
+const pts = d3.scaleLinear()
+      .domain(domain.pts)
+      .range([margin.left, margin.left + full_scale ]);
 
-  // Gender Male/Female
-  const gender = d3.scaleLinear()
-        .domain(domain.gender)
-        .range([starts.gender, starts.gender + full_scale * scales.gender]);
+// Gender Male/Female
+const gender = d3.scaleLinear()
+      .domain(domain.gender)
+      .range([starts.gender, starts.gender + full_scale * scales.gender]);
 
 
-  // Brain Mean dose
-  const brain_dmean = d3.scaleLinear()
-        .domain(domain.brain_dmean)
-        .range([starts.brain_dmean, starts.brain_dmean + full_scale * scales.brain_dmean]);
+// Brain Mean dose
+const brain_dmean = d3.scaleLinear()
+      .domain(domain.brain_dmean)
+      .range([starts.brain_dmean, starts.brain_dmean + full_scale * scales.brain_dmean]);
+
 
 // Baseline (Pre-CCRT ALC x10)
-  const baseline = d3.scaleLinear()
-        .domain(domain.baseline)
-        .range([starts.baseline, starts.baseline + full_scale * scales.baseline]);
+const baseline = d3.scaleLinear()
+      .domain(domain.baseline)
+      .range([starts.baseline, starts.baseline + full_scale * scales.baseline]);
 
-  const tot_pts = d3.scaleLinear()
-        .domain(domain.tot_pts)
-        .range([starts.tot_pts, starts.tot_pts + full_scale]);
+const tot_pts = d3.scaleLinear()
+      .domain(domain.tot_pts)
+      .range([starts.tot_pts, starts.tot_pts + full_scale]);
 
 const slp = d3.scaleLinear()
-        .domain(domain.slp)
-        .range([starts.slp, starts.slp + full_scale * scales.slp]);
+      .domain(domain.slp)
+      .range([starts.slp, starts.slp + full_scale * scales.slp]);
 
 
-  // Add the x-axis.
-  svg.append("g")
+// Add the x-axis.
+svg.append("g")
     .attr("transform", `translate(0,${margin.top})`)
     .call(d3.axisBottom(pts))
     .append("text")
     .attr("class", "axis-title")
     .attr("transform", "rotate(0)")
-        .attr("x", margin.left-20)
-        .attr("dy", ".71em")
-        .style("text-anchor", "end")
-        .attr("fill", "#5D6971")
+    .attr("x", margin.left-20)
+    .attr("dy", ".71em")
+    .style("text-anchor", "end")
+    .attr("fill", "#5D6971")
     .text("Points");
 
 
-  svg.append("g")
+svg.append("g")
     .attr("transform", `translate(0,${margin.top+gap})`)
     .call(d3.axisBottom(gender).tickValues([0,1]))
     .append("text")
     .attr("class", "axis-title")
     .attr("transform", "rotate(0)")
-        .attr("x", margin.left-20)
-        .attr("dy", ".71em")
-        .style("text-anchor", "end")
-        .attr("fill", "#5D6971")
+    .attr("x", margin.left-20)
+    .attr("dy", ".71em")
+    .style("text-anchor", "end")
+    .attr("fill", "#5D6971")
     .text("Gender");
 
-  svg.append("g")
+svg.append("g")
     .attr("transform", `translate(0,${margin.top+2.0*gap})`)
     .call(d3.axisBottom(brain_dmean))
     .append("text")
     .attr("class", "axis-title")
     .attr("transform", "rotate(0)")
-        .attr("x", margin.left-20)
-        .attr("dy", ".71em")
-        .style("text-anchor", "end")
-        .attr("fill", "#5D6971")
+    .attr("x", margin.left-20)
+    .attr("dy", ".71em")
+    .style("text-anchor", "end")
+    .attr("fill", "#5D6971")
     .text("Brain Dmean (Gy)");
 
-  svg.append("g")
+svg.append("g")
     .attr("transform", `translate(0,${margin.top+3*gap})`)
     .call(d3.axisBottom(baseline))
     .append("text")
     .attr("class", "axis-title")
     .attr("transform", "rotate(0)")
-        .attr("x", margin.left-20)
-        .attr("dy", ".71em")
-        .style("text-anchor", "end")
-        .attr("fill", "#5D6971")
+    .attr("x", margin.left-20)
+    .attr("dy", ".71em")
+    .style("text-anchor", "end")
+    .attr("fill", "#5D6971")
     .text("Baseline ALC (x1000)");
 
-  svg.append("g")
+svg.append("g")
     .attr("transform", `translate(0,${margin.top+4*gap})`)
     .call(d3.axisBottom(tot_pts))
     .append("text")
     .attr("class", "axis-title")
     .attr("transform", "rotate(0)")
-        .attr("x", margin.left-20)
-        .attr("dy", ".71em")
-        .style("text-anchor", "end")
-        .attr("fill", "#5D6971")
-        .text("Total Points");
+    .attr("x", margin.left-20)
+    .attr("dy", ".71em")
+    .style("text-anchor", "end")
+    .attr("fill", "#5D6971")
+    .text("Total Points");
 
-  svg.append("g")
+svg.append("g")
     .attr("transform", `translate(0,${margin.top+5*gap})`)
     .call(d3.axisBottom(slp))
     .append("text")
     .attr("class", "axis-title")
     .attr("transform", "rotate(0)")
-        .attr("x", margin.left-20)
-        .attr("dy", ".71em")
-        .style("text-anchor", "end")
-        .attr("fill", "#5D6971")
-        .text("Probability of SLP");
+    .attr("x", margin.left-20)
+    .attr("dy", ".71em")
+    .style("text-anchor", "end")
+    .attr("fill", "#5D6971")
+    .text("Probability of SLP");
+
+
+
+
+function gender_points(ig){
+//    1  0.00000 (male)
+//    2 36.00951 (female)
+    if (ig){
+        return 36.00951
+    }else{
+        return 0.0
+    }
+}
+
+function dose_points(ig){
+    //[1] "points = 2.22222222 * Brain_Dmean (gy) + -11.11111111"
+    return 2.22222222 * ig + -11.11111111
+}
+
+function baseline_points(ig){
+    //**Baseline ALC 기준은 x10^3/uL 기준.
+    //[1] "points = 0 * Baseline_ALC ^3 + 0 * Baseline_ALC ^2 + -12.51118092 * Baseline_ALC + 87.57826641"
+    return -12.51118092 * ig + 87.57826641
+}
+
+function total_points(is_female, dose, alc){
+    return gender_points(is_female) + dose_points(dose) + baseline_points(alc)
+}
+
+function probability(points){
+    //[[5]] SLP 발생 확률 --> 추후에 x 100 (%) 으로 환산해야함.
+    //[1] "Probability of SLP = 9e-08 * points ^3 + -5.93e-06 * points ^2 + 4.978e-05 * points + 0.01104449"
+    return  9e-08 * (points**3) + -5.93e-06 * (points**2) + 4.978e-05 * points + 0.01104449
+}
+
+var input_gender = true //female:true, male:false
+var input_dose   = 45.0 
+var input_baseline = 5.0 
+var input_total = total_points(input_gender, input_dose, input_baseline)
+var input_slp = probability(input_total)
+// -----------------------------------------//
+// Gender and its point
+// -----------------------------------------//
+var output_gender = gender_points(input_gender)
+var pts_gender    = pts(output_gender)
+
+svg.append("circle")
+    .attr("cx", gender(input_gender))
+    .attr("cy", margin.top+1*gap)
+    .attr("stroke", "red")
+    .attr("stroke-width", 2)
+    .attr("fill", "red")
+    .attr("r", 5)
+
+svg.append("circle")
+    .attr("cx", pts_gender)
+    .attr("cy", margin.top+0*gap)
+    .attr("stroke", "red")
+    .attr("stroke-width", 2)
+    .attr("fill", "red")
+    .attr("r", 5)
+
+svg.append("line")
+    .style("stroke", "red")
+    .attr("x1" , pts_gender)
+    .attr("y1" , margin.top+0.0*gap )
+    .attr("x2" , pts_gender)
+    .attr("y2" , margin.top+1*gap)
+
+// -----------------------------------------//
+// Brain Dmean and its point
+// -----------------------------------------//
+var output_dose = dose_points(input_dose)
+var pts_dose    = pts(output_dose)
+
+svg.append("circle")
+    .attr("cx", brain_dmean(input_dose))
+    .attr("cy", margin.top+2*gap)
+    .attr("stroke", "green")
+    .attr("r", 5)
+
+svg.append("circle")
+    .attr("cx", pts_dose)
+    .attr("stroke", "green")
+    .attr("cy", margin.top+0*gap)
+    .attr("r", 5)
+
+svg.append("line")
+    .style("stroke", "green")
+    .attr("x1" , pts_dose)
+    .attr("y1" , margin.top+0.0*gap )
+    .attr("x2" , pts_dose)
+    .attr("y2" , margin.top+2*gap)
+
+// -----------------------------------------//
+// Baseline and its point
+// -----------------------------------------//
+var output_baseline = baseline_points(input_baseline)
+var pts_baseline    = pts(output_baseline)
+
+svg.append("circle")
+    .attr("stroke", "blue")
+    .attr("cx", baseline(input_baseline))
+    .attr("cy", margin.top+3*gap)
+    .attr("r", 5)
+
+svg.append("circle")
+    .attr("stroke", "blue")
+    .attr("cx", pts(baseline_points(input_baseline)))
+    .attr("cy", margin.top+0*gap)
+    .attr("r", 5)
+
+svg.append("line")
+    .style("stroke", "blue")
+    .attr("x1" , pts_baseline)
+    .attr("y1" , margin.top+0.0*gap )
+    .attr("x2" , pts_baseline)
+    .attr("y2" , margin.top+3*gap)
+
+
+// -----------------------------------------//
+// Total points
+// -----------------------------------------//
+svg.append("circle")
+    .attr("cx", tot_pts(input_total))
+    .attr("cy", margin.top+4*gap)
+    .attr("r", 5)
+
+// three lines to total point
+svg.append("line")
+    .style("stroke", "red")
+    .attr("x1" , pts_gender)
+    .attr("y1" , margin.top+1*gap)
+    .attr("x2" , tot_pts(input_total))
+    .attr("y2" , margin.top+4*gap )
+
+svg.append("line")
+    .style("stroke", "green")
+    .attr("x1" , pts_dose)
+    .attr("y1" , margin.top+2*gap)
+    .attr("x2" , tot_pts(input_total))
+    .attr("y2" , margin.top+4*gap )
+
+svg.append("line")
+    .style("stroke", "blue")
+    .attr("x1" , pts_baseline)
+    .attr("y1" , margin.top+3*gap)
+    .attr("x2" , tot_pts(input_total))
+    .attr("y2" , margin.top+4*gap )
+
+
+// -----------------------------------------//
+// Probability of SLP
+// -----------------------------------------//
+svg.append("circle")
+    .attr("cx", slp(input_slp))
+    .attr("cy", margin.top+5*gap)
+    .attr("r", 5)
+
+svg.append("line")
+    .style("stroke", "black")
+    .attr("x1" , tot_pts(input_total))
+    .attr("y1" , margin.top+4*gap )
+    .attr("x2" , slp(input_slp))
+    .attr("y2" , margin.top+5*gap)
+    .attr("marker-end", "url(#triangle)");
 
 
 function update_slp(){
     //var m = d3.selectAll(("input[name='gender']")); //error
+    console.log("update")
     /*
 
-    Console.Log(m)
-    */
+
+     */
 }
 
-d3.select("#brain_dose").on("input", function(){ update_slp(); console.log("Brain dose"+this.value)  });
-d3.select("#baseline").on("input", function(){ update_slp(); console.log("BaselineALC"+this.value)  });
-d3.select("#gender").on("click", function(){ update_slp();console.log("gender"+this.value)  });
+d3.select("#male").on("change", function(){
+    update_slp();console.log("gender"+this.value)
+});
+d3.select("#female").on("change", function(){
+    update_slp();console.log("gender"+this.value)
+});
+d3.select("#brain_dose").on("input", function(){
+    console.log("Brain dose"+this.value)
+});
+d3.select("#baseline").on("input", function(){
+    console.log("BaselineALC"+this.value)
+});
+
 
 // Return the SVG element.
 plot_div.append(svg.node());
